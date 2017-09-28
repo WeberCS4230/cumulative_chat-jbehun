9293036c4d70b5cefa36158662f55125eabb27d6
Justin Behunin <>
2017-09-28 16:48:15 -0600

    Created class with main method and added get/set methods with toString to Student

--- src/Student.java
+++ src/Student.java
@@ -1,12 +1,36 @@
 
-public class Student 
-{
-private String firstName, lastName;
-private int score;
+public class Student {
+	private String firstName, lastName;
+	private int score;
 
-public Student(String first, String last)
-{
-	firstName = first;
-	lastName = last;
-}
+	public Student(String first, String last) {
+		firstName = first;
+		lastName = last;
+	}
+
+	//default get set methods.
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	@Override
+	public String toString() {
+		return firstName + lastName;
+	}
+	
+	
+	
+	
 }
---
+++ src/StudentChat.java
@@ -0,0 +1,9 @@
+
+public class StudentChat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
