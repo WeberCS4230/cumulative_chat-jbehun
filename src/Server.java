1dca311916b697d4442621025725812567735106
Justin Behunin <>
2017-10-11 00:14:04 -0600

    Created serversocket with basic printerwriter and buffered read. Added names to a list to track incoming user names. Send a connection response back to the client.

--- src/Server.java
+++ src/Server.java
@@ -1,9 +1,35 @@
+import java.io.*;
+import java.net.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Server {
 
-	public static void main(String[] args) {
-		
+	private ServerSocket ss;
+	private Set<String> clients = new HashSet<String>();
 
+	public Server() {
+		try {
+			ss = new ServerSocket(8090);
+			while (true) {
+				Socket s = ss.accept();
+				BufferedReader read = new BufferedReader(new InputStreamReader(s.getInputStream()));
+				PrintWriter write = new PrintWriter(s.getOutputStream());
+				if(clients.add(read.readLine())) {
+					write.println("ACK\n");
+					write.flush();
+				}else {
+					write.println("Decline\n");
+					write.flush();
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		new Server();
 	}
 
 }
