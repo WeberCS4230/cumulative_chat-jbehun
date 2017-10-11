57768df50cfbf9a0bd41cf9e23b825629b7846a6
Justin Behunin <>
2017-10-11 00:14:46 -0600

    Set up a basic client and tested if it connects and if it rejects muliple clients with the same name.

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
 
 public class Client {
 
+	private Socket s = null;
+	private String name;
+
+	public Client(String n) {
+		try {
+			s = new Socket("localhost", 8090);
+			BufferedReader read = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			PrintWriter write = new PrintWriter(s.getOutputStream());
+			write.write(n + "\n");
+			write.flush();
+			String responseString = read.readLine();
+			System.out.println(responseString);
+			if (responseString.equals("ACK")) {
+				System.out.println("Connected");
+			}else if(responseString.equals("Decline")){
+				System.out.println("Denied");
+			}
+			else  {
+				System.out.println("Connection Failed");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				s.getInputStream().close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				s.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+
 	public static void main(String[] args) {
-		
+				new Client("John");
+
 
 	}
 
